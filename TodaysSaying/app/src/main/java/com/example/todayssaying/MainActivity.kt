f22fdc67620cfd94560fcb2a62634ef648614d3a
HongEunho <ghdwns315@naver.com>
2021-07-04 15:59:13 +0900

    update: Today's Saying
    update: Today's Saying

--- TodaysSaying/app/src/main/java/com/example/todayssaying/MainActivity.kt
+++ TodaysSaying/app/src/main/java/com/example/todayssaying/MainActivity.kt
@@ -2,10 +2,89 @@ package com.example.todayssaying
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewPager: ViewPager2 by lazy{
+        findViewById<ViewPager2>(R.id.viewPager)
+    }
+
+    private val progressBar: ProgressBar by lazy{
+        findViewById<ProgressBar>(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initData()
+        initViews()
+    }
+
+    private fun initViews() {
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+                position == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha = 1F - 2 * position.absoluteValue // 점점 흐려지고 나타나는 효
+                }
+            }
+        }
+    }
+
+    private fun initData() {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 0
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+            if(it.isSuccessful){
+                val quotes = parseQuotesJson(remoteConfig.getString("quotes"))
+                val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
+
+                displayQuotesPager(quotes, isNameRevealed)
+
+            }
+        }
+    }
+
+    private fun parseQuotesJson(json: String): List<Quote> {
+        val jsonArray = JSONArray(json)
+        var jsonList = emptyList<JSONObject>()
+        for(index in 0 until jsonArray.length()){
+            val jsonObject = jsonArray.getJSONObject(index)
+            jsonObject?.let{
+                jsonList += it
+            }
+        }
+
+        return jsonList.map{
+            Quote(
+                quote = it.getString("quote"),
+                name = it.getString("name")
+            )
+        }
+    }
+
+    private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
+        viewPager.adapter = QuotesPagerAdapter(quotes, isNameRevealed)
+        viewPager.setCurrentItem(quotes.size / 2, false)
     }
 }
--- TodaysSaying/app/src/main/java/com/example/todayssaying/QuotesPagerAdapter.kt
+++ TodaysSaying/app/src/main/java/com/example/todayssaying/QuotesPagerAdapter.kt
@@ -1,17 +1,20 @@
 package com.example.todayssaying
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class QuotesPagerAdapter(private val quotes: List<Quote>): RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
+class QuotesPagerAdapter(private val quotes: List<Quote>, private val isNameRevealed: Boolean
+): RecyclerView.Adapter<QuotesPagerAdapter.QuoteViewHolder>() {
 
-    override fun getItemCount() = quotes.size
+    override fun getItemCount() = Int.MAX_VALUE
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
-        holder.bind(quotes[position])
+        val actualPosition = position % quotes.size
+        holder.bind(quotes[actualPosition], isNameRevealed)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder =
@@ -22,9 +25,16 @@ class QuotesPagerAdapter(private val quotes: List<Quote>): RecyclerView.Adapter<
         private val quoteTextView: TextView = itemView.findViewById(R.id.quoteTextView)
         private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
 
-        fun bind(quote: Quote) {
-            quoteTextView.text = quote.quote
-            nameTextView.text = quote.name
+        @SuppressLint("SetTextI18n")
+        fun bind(quote: Quote, isNameRevealed: Boolean) {
+            quoteTextView.text = "\"${quote.quote}\""
+
+            if(isNameRevealed) {
+                nameTextView.text = "-${quote.name}"
+                nameTextView.visibility = View.VISIBLE
+            } else {
+                nameTextView.visibility = View.GONE
+            }
         }
     }
 }
