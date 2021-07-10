e702697c544ff815db10c353dd35c9cf4da320b5
HongEunho <ghdwns315@naver.com>
2021-07-10 17:30:26 +0900

    create: TestApp
    create: TestApp

---
+++ TestApp/app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val button: Button by lazy{
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
