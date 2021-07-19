da3f823d0187a6adba1cdf6c7ffe3c82db91b792
HongEunho <ghdwns315@naver.com>
2021-07-20 01:22:06 +0900

    update: Dagger Gallery

--- DaggerGallery/app/src/main/java/com/example/daggergallery/GalleryActivity.kt
+++ DaggerGallery/app/src/main/java/com/example/daggergallery/GalleryActivity.kt
@@ -5,26 +5,27 @@ import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.daggergallery.di.ApplicationComponent
 import com.example.daggergallery.di.GalleryComponent
+import dagger.android.support.DaggerAppCompatActivity
 import java.util.jar.Manifest
 import javax.inject.Inject
 
-class GalleryActivity : AppCompatActivity() {
+class GalleryActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var viewModel: GalleryViewModel
 
     private val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        // ...
+        if (it) { init() } else { finish() }
     }
 
     lateinit var component: GalleryComponent
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        /* AndroidInject를 사용했기 때문에 서브 컴포넌트 생성 및 인젝션 하는 코드를 작성하지 않아도 됨
         component = MyApplication.appComponent.getGalleryComponentFactory().create(this)
-        component.inject(this)
+        component.inject(this)*/
 
         super.onCreate(savedInstanceState)
-
         launcher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         setContentView(R.layout.activity_gallery)
     }
--- DaggerGallery/app/src/main/java/com/example/daggergallery/di/ApplicationModule.kt
+++ DaggerGallery/app/src/main/java/com/example/daggergallery/di/ApplicationModule.kt
@@ -2,12 +2,26 @@ package com.example.daggergallery.di
 
 import android.app.Application
 import android.content.Context
+import com.example.daggergallery.GalleryActivity
 import dagger.Binds
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
 
-@Module(subcomponents = [GalleryComponent::class])
+@Module
 abstract class ApplicationModule {
 
     @Binds
     abstract fun bindsContext(application: Application): Context
+
+    // AndroidInjector를 사용하기 때문에 더 이상 서브 컴포넌트를 정의할 필요가 없다.
+    // 따라서 GalleryComponent 파일을 지워도 된다.
+    // 대신 @ContributesAndroidInjector를 사용하여 서브 컴포넌트 정의를 대신함.
+
+    // Dagger는 @ContributesAndroidInjector로부터 얻은 메타데이터를 통해
+    // 서브 컴포넌트를 컴파일 타임에 생성
+    // 반드시 상위 컴포넌트의 모듈 내 추상 메소드로 정의
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [GalleryModule::class])
+    abstract fun galleryActivity(): GalleryActivity
 }
