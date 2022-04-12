50106b31d7e7a8f93219fc982493f82db9734112
HongEunho <ghdwns315@naver.com>
2022-04-12 22:47:35 +0900

    feat: Dto까지 완료
    feat: Dto까지 완료

--- YoutubeCloneApp/app/src/main/java/com/example/youtubecloneapp/PlayerFragment.kt
+++ YoutubeCloneApp/app/src/main/java/com/example/youtubecloneapp/PlayerFragment.kt
@@ -2,8 +2,10 @@ package com.example.youtubecloneapp
 
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import com.example.youtubecloneapp.databinding.FragmentPlayerBinding
+import kotlin.math.abs
 
 class PlayerFragment: Fragment(R.layout.fragment_player) {
 
@@ -14,5 +16,37 @@ class PlayerFragment: Fragment(R.layout.fragment_player) {
 
         val fragmentPlayerBinding = FragmentPlayerBinding.bind(view)
         binding = fragmentPlayerBinding
+
+        fragmentPlayerBinding.playerMotionLayout.setTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) { }
+
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                binding?.let {
+                    (activity as MainActivity).also { mainActivity ->
+                        mainActivity.findViewById<MotionLayout>(R.id.mainMotionLayout).progress = abs(progress)
+                    }
+                }
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        binding = null
     }
 }
---
+++ YoutubeCloneApp/app/src/main/java/com/example/youtubecloneapp/dto/VideoDto.kt
@@ -0,0 +1,7 @@
+package com.example.youtubecloneapp.dto
+
+import com.example.youtubecloneapp.model.VideoModel
+
+data class VideoDto(
+    val videos: List<VideoModel>
+)
